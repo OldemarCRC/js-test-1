5edd22bd191def49cb541e04320187698435e1b4
vickneee <victoria.vavulina@outlook.com>
2023-03-08 11:58:14 +0200

    JS changes

--- script1.js
+++ script1.js
@@ -1,7 +1,5 @@
-// <script type="text/javascript">
 const age = prompt("Please enter your age");
 if(age>=18)
     document.write("You are an adult <br />");
 if(age<18)
     document.write("You are underage <br />");
-// </script>
--- script2.js
+++
@@ -1,6 +0,0 @@
-const age = prompt("Please enter your age");
-if(age>=18)
-    document.write("You are an adult <br />");
-if(age<18)
-    document.write("You are underage <br />");
-
