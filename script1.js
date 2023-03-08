505cbb16ecf3af848919c83cf2c95d807eae0655
vickneee <victoria.vavulina@outlook.com>
2023-03-08 13:13:14 +0200

    Indentation corr

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 const age = prompt("Please enter your age");
-if(age>=18)
+if (age >= 18)
     document.write("You are an adult <br />");
-if(age<18)
+if (age < 18)
     document.write("You are underage <br />");
