641752907df449bf360335e703dff887c6ab94f4
vickneee <victoria.vavulina@outlook.com>
2023-03-09 03:04:05 +0200

    JS new function, Margin

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello World!")
+
+const welcome = 'Welcome!';
+const h1 = document.querySelector(".heading-primary")
+console.log(welcome)
+console.log(h1)
+h1.addEventListener("click", function () {
+    h1.textContent = welcome;
+    h1.style.textShadow = "0.8rem 1.4rem 0.8rem #ddd";
+    h1.style.fontSize = "5.2rem";
+});
--- script1.js
+++ script1.js
@@ -3,3 +3,5 @@ if (age >= 18)
     document.write("<h2>You are an adult</h2>");
 if (age < 18)
     document.write("<h2>You are underage</h2>");
+
+
