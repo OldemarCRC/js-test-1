a9de1be3a543fa92e4abe1804aac285284f86820
vickneee <victoria.vavulina@outlook.com>
2023-03-09 03:47:38 +0200

    Font-size

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ console.log(h1)
 h1.addEventListener("click", function () {
     h1.textContent = welcome;
     h1.style.textShadow = "0.8rem 1.4rem 0.8rem #ddd";
-    h1.style.fontSize = "5.2rem";
+    h1.style.fontSize = "4.8rem";
 });
