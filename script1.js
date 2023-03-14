23c0d123fedd9ba2687e7dbb378ab23b86632845
vickneee <victoria.vavulina@outlook.com>
2023-03-14 21:16:41 +0200

    Fixes in H1, Index2, Script2

--- script1.js
+++ script1.js
@@ -1,4 +1,5 @@
 const age = prompt("Please enter your age");
+
 if (age >= 18)
     document.write("<h2>You are an adult</h2>");
 if (age < 18)
---
+++ script2.js
@@ -0,0 +1,6 @@
+let age = Number(document.getElementById("age").value);
+if (25 <= age && age >= 16) 
+    document.write("<h2>You can work in McDonald's!</h2>")
+// else (16 < age && age > 26) 
+// if document.write("<h2>You can´t work in McDonald's!</h2>")
+
