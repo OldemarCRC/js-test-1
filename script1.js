eac21e566dc9d21fcc69f12236a35c7d502014ac
vickneee <victoria.vavulina@outlook.com>
2023-03-15 01:03:37 +0200

    Script1 corr

--- script1.js
+++ script1.js
@@ -1,8 +1,20 @@
-const age = prompt("Please enter your age");
+// const prompt=require("prompt-sync")({sigint:true});
+
+const age = prompt("Please enter your age: ");
 
 if (age >= 18)
-    document.write("<h2>You are an adult</h2>");
+    document.write("<h1>You are an adult</h1>");
 if (age < 18)
-    document.write("<h2>You are underage</h2>");
+    document.write("<h1>You are underage</h1>");
 
+if (age <= 25 && age >= 16)
+    document.write("<h2>You can work in McDonald's!</h2>");
+else 
+    document.write("<h2>You can´t work in McDonald's!</h2>");
+
+// if (age <= 25 && age >= 16) {
+//     console.log("You can work in McDonald's!")
+// } else {
+//     console.log("You can´t work in McDonald's!")
+// }
 
--- script2.js
+++
@@ -1,8 +0,0 @@
-const prompt=require("prompt-sync")({sigint:true});
-
-const age = prompt("Please enter your age: ");
-if (age <= 25 && age >= 16) {
-    console.log("You can work in McDonald's!")
-} else {
-    console.log("You can´t work in McDonald's!")
-}
