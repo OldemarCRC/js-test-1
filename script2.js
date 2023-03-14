8437aae7362755afc5ca6c35ceb488f0e96affec
vickneee <victoria.vavulina@outlook.com>
2023-03-15 00:29:47 +0200

    Script2

--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
-let age = Number(document.getElementById("age").value);
-if (25 <= age && age >= 16) 
+const age = Number(document.getElementById("age").value);
+if (age <= 25 && age >= 16) {
+    console.log("You can work in McDonald's!")
     document.write("<h2>You can work in McDonald's!</h2>")
-// else (16 < age && age > 26) 
-// if document.write("<h2>You can´t work in McDonald's!</h2>")
-
+} else if (16 < age && age > 26) {
+    document.write("<h2>You can´t work in McDonald's!</h2>")
+}
